380f6e1ec44198a7913e6f71d0c34e1238043264
Tamas Sopronyi <tommysoprano@gmail.com>
2023-03-14 20:54:00 +0100

    Updated OpenFL sample app

--- test/openfl/Network/Source/Main.hx
+++ test/openfl/Network/Source/Main.hx
@@ -1,3 +1,33 @@
+/*
+ *  Copyright (C) 2016-present Prominic.NET, Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the Server Side Public License, version 1,
+ *  as published by MongoDB, Inc.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  Server Side Public License for more details.
+ *
+ *  You should have received a copy of the Server Side Public License
+ *  along with this program. If not, see
+ *
+ *  http://www.mongodb.com/licensing/server-side-public-license
+ *
+ *  As a special exception, the copyright holders give permission to link the
+ *  code of portions of this program with the OpenSSL library under certain
+ *  conditions as described in each individual source file and distribute
+ *  linked combinations including the program with the OpenSSL library. You
+ *  must comply with the Server Side Public License in all respects for
+ *  all of the code used other than as permitted herein. If you modify file(s)
+ *  with this exception, you may extend this exception to your version of the
+ *  file(s), but you are not obligated to do so. If you do not wish to do so,
+ *  delete this exception statement from your version. If you delete this
+ *  exception statement from all source files in the program, then also delete
+ *  it in the license file.
+ */
+
 package;
 
 import feathers.controls.Application;
@@ -18,10 +48,14 @@ import prominic.sys.SysTools;
 import prominic.sys.io.Process;
 import prominic.sys.io.process.AbstractProcess;
 import prominic.sys.io.process.CallbackProcess;
+import prominic.sys.network.ICMPSocket;
 import prominic.sys.network.Network;
 
 class Main extends Application {
 
+    var buttonStartPing:Button;
+    var buttonStopPing:Button;
+    var icmpSocket:ICMPSocket;
     var inputHost:TextInput;
     var inputProcess:TextInput;
     var textArea:TextArea;
@@ -59,6 +93,15 @@ class Main extends Application {
 
         inputHost = new TextInput( "www.google.com" );
         group.addChild( inputHost );
+
+        buttonStartPing = new Button( "Start Ping" );
+        buttonStartPing.addEventListener( TriggerEvent.TRIGGER, _buttonStartPingTriggered );
+        group.addChild( buttonStartPing );
+
+        buttonStopPing = new Button( "Stop Ping" );
+        buttonStopPing.enabled = false;
+        buttonStopPing.addEventListener( TriggerEvent.TRIGGER, _buttonStopPingTriggered );
+        group.addChild( buttonStopPing );
 
         textArea = new TextArea();
         textArea.layoutData = new VerticalLayoutData( 100, 100 );
@@ -140,6 +183,73 @@ class Main extends Application {
     function _onProcessStop( ?process:AbstractProcess ) {
 
         Logger.info( "Process stopped" );
+
+    }
+
+    function _buttonStartPingTriggered( e:TriggerEvent ) {
+
+        if ( icmpSocket == null ) {
+
+            icmpSocket = new ICMPSocket( inputHost.text );
+            icmpSocket.onHostError= onHostError;
+            icmpSocket.onPing = onPing;
+            icmpSocket.onPingFinished = onPingFinished;
+            icmpSocket.onError = onPingError;
+            icmpSocket.onTimeout = onTimeout;
+            icmpSocket.ping( 0 );
+
+            buttonStopPing.enabled = true;
+            buttonStartPing.enabled = false;
+
+        }
+
+    }
+
+    function _buttonStopPingTriggered( e:TriggerEvent ) {
+
+        if ( icmpSocket != null ) {
+
+            icmpSocket.close();
+            icmpSocket = null;
+
+            buttonStopPing.enabled = false;
+            buttonStartPing.enabled = true;
+
+        }
+
+    }
+
+    function onHostError( socket:ICMPSocket ) {
+
+        Logger.error( 'Host error on: ${socket.hostname}' );
+        
+        _buttonStopPingTriggered( null );
+
+    }
+
+    function onPing( socket:ICMPSocket ) {
+
+        Logger.info( 'Ping successful on: ${socket.hostname}, time (ms): ${socket.lastPingTime}' );
+
+    }
+
+    function onPingFinished( socket:ICMPSocket ) {
+
+        Logger.info( 'Ping finished on: ${socket.hostname}' );
+
+        _buttonStopPingTriggered( null );
+
+    }
+
+    function onPingError( socket:ICMPSocket ) {
+
+        Logger.error( 'Ping error on: ${socket.hostname}' );
+
+    }
+
+    function onTimeout( socket:ICMPSocket ) {
+
+        Logger.warning( 'Ping timeout on: ${socket.hostname}' );
 
     }
 
