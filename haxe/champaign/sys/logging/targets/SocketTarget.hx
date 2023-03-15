f3128244730784c74d275153d0d1a580cadf8d84
Tamas Sopronyi <tommysoprano@gmail.com>
2023-03-15 15:05:54 +0100

    Create SocketTarget.hx

---
+++ haxe/champaign/sys/logging/targets/SocketTarget.hx
@@ -0,0 +1,112 @@
+/*
+ *  Copyright (C) 2016-present Prominic.NET, Inc.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the Server Side Public License, version 1,
+ *  as published by MongoDB, Inc.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  Server Side Public License for more details.
+ *
+ *  You should have received a copy of the Server Side Public License
+ *  along with this program. If not, see
+ *
+ *  http://www.mongodb.com/licensing/server-side-public-license
+ *
+ *  As a special exception, the copyright holders give permission to link the
+ *  code of portions of this program with the OpenSSL library under certain
+ *  conditions as described in each individual source file and distribute
+ *  linked combinations including the program with the OpenSSL library. You
+ *  must comply with the Server Side Public License in all respects for
+ *  all of the code used other than as permitted herein. If you modify file(s)
+ *  with this exception, you may extend this exception to your version of the
+ *  file(s), but you are not obligated to do so. If you do not wish to do so,
+ *  delete this exception statement from your version. If you delete this
+ *  exception statement from all source files in the program, then also delete
+ *  it in the license file.
+ */
+
+package champaign.sys.logging.targets;
+
+import champaign.core.logging.Logger;
+import champaign.core.logging.targets.AbstractLoggerTarget;
+import haxe.Json;
+import sys.net.Socket;
+
+class SocketTarget extends AbstractLoggerTarget {
+
+    public var socket( default, null ):Socket;
+
+    public function new( socket:Socket, logLevel:LogLevel = LogLevel.Info, printTime:Bool = false, machineReadable:Bool = true ) {
+
+        super( logLevel, printTime, machineReadable );
+
+        this.socket = socket;
+
+    }
+
+    function loggerFunction( message:LoggerFormattedMessage ) {
+
+        if ( !enabled ) return;
+
+        if ( message.level > _logLevel ) return;
+
+        if ( _machineReadable ) {
+
+            try {
+
+                this.socket.write( Json.stringify( message ) + "\n" );
+
+            } catch ( e ) { }
+
+        } else {
+
+            var m:String = '';
+
+            if ( _printTime ) m = '[${message.date}]';
+            if ( message.entity != null ) m += '[${message.entity}]';
+
+            // Level
+            switch message.level {
+
+                case LogLevel.Fatal:
+                    m += '[FATAL]';
+
+                case LogLevel.Error:
+                    m += '[ERROR]';
+
+                case LogLevel.Warning:
+                    m += '[WARNING]';
+
+                case LogLevel.Info:
+                    m += '[INFO]';
+
+                case LogLevel.Debug:
+                    m += '[DEBUG]';
+
+                case LogLevel.Verbose:
+                    m += '[VERBOSE]';
+
+                default:
+
+            }
+
+            if ( message.source != null ) m += '[${message.source}]';
+
+            m += ' ${message.message}';
+
+            if ( message.custom != null ) m += ' [Custom: ${message.custom}]';
+
+            try {
+
+                this.socket.write( m + "\n" );
+
+            } catch ( e ) { }
+
+        }
+
+    }
+    
+}
