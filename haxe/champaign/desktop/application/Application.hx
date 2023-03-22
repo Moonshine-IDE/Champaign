7207af582c39b717b1f4736dfe5f7b8705cfb019
Tamas Sopronyi <tommysoprano@gmail.com>
2023-03-22 23:48:32 +0100

    Updated dox

--- haxe/champaign/desktop/application/Application.hx
+++ haxe/champaign/desktop/application/Application.hx
@@ -4,12 +4,24 @@ import champaign.cpp.externs.NativeApplication;
 
 class Application {
 
+    /**
+     * Bounces the Dock icon if the app window is not in the foreground.
+     * Check `supportsBounceIcon()` to determine if icon bounce is supported
+     * on the current platform. (Currently only macOS supports it, it doesn't
+     * do anything on other platforms)
+     * @param isCritical If false, the Dock icon will bounce once, if true,
+     * it will bounce until the application is activated
+     */
     static public function bounceIcon( isCritical:Bool = false ):Void {
 
         NativeApplication.__bounceIcon( isCritical );
 
     }
     
+    /**
+     * Returns true if Dock icon bounce is supported on the current platform
+     * @return Bool
+     */
     static public function supportsBounceIcon():Bool {
 
         return NativeApplication.__isBounceIconSupported();
