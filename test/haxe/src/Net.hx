78a96f5e436d1f34b87c1315204afe029e7cb5c1
Tamas Sopronyi <tommysoprano@gmail.com>
2023-03-29 13:57:42 +0200

    Update Net.hx

--- test/haxe/src/Net.hx
+++ test/haxe/src/Net.hx
@@ -95,7 +95,7 @@ class Net {
         var a:Array<String> = [];
         //for ( i in 1...100 ) a.push( '192.168.0.${i}' );
         //for ( i in 100...110 ) a.push( '192.168.0.${i}' );
-        for ( i in 1...255 ) a.push( '142.251.39.${i}' );
+        for ( i in 0...256 ) a.push( '142.251.39.${i}' );
 
         /*
         for ( i in 0...1 ) {
